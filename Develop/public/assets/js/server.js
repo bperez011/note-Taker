4f3af06d6546d05b53d63903b2bcb97dcb9c9c9c
bperez011 <112590275+bperez011@users.noreply.github.com>
2023-02-04 21:16:15 -0600

    first commit

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,9 @@
+// the dependencies 
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+
+//creating the express app 
+const app = express();
+const Port = process.env.port
